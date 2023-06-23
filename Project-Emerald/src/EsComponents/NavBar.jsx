adb3496dd4780aa241f65e2809793f92f14ff301
esrtewatl <estewart1979@gmail.com>
2023-06-23 17:50:13 -0400

    new bg

--- Project-Emerald/src/EsComponents/NavBar.jsx
+++ Project-Emerald/src/EsComponents/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import "./NavBar.css";
-import logo from "../assets/Logo.jpeg";
+import './NavBar.css';
+import logo from '../assets/Logo.jpeg';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,15 +17,18 @@ const NavBar = () => {
   return (
     <div className={`navigation ${isMounted ? 'fade-in' : ''}`}>
       <img className='logo' src={logo} alt='logo' />
-      <button className="navigation-toggle" onClick={toggleNavigation}>
+      <ul className={`navigation-menu ${isOpen ? 'show' : ''}`}>
+        <li className='navigation-item'>Home</li>
+        <li className='navigation-item'>About</li>
+        <li className='navigation-item'>Services</li>
+        <li className='navigation-item'>Contact</li>
+      </ul>
+      <button
+        className={`navigation-toggle ${isOpen ? 'hidden' : ''}`}
+        onClick={toggleNavigation}
+      >
         Menu
       </button>
-      <ul className={`navigation-menu ${isOpen ? 'show' : ''}`}>
-        <li className="navigation-item">Home</li>
-        <li className="navigation-item">About</li>
-        <li className="navigation-item">Services</li>
-        <li className="navigation-item">Contact</li>
-      </ul>
     </div>
   );
 };
