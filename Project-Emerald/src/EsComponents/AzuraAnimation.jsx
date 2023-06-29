cd3072c3191950913e26ba800cb70ebfc3f070cf
esrtewatl <estewart1979@gmail.com>
2023-06-29 17:46:27 -0400

    Animation

--- Project-Emerald/src/EsComponents/AzuraAnimation.jsx
+++ Project-Emerald/src/EsComponents/AzuraAnimation.jsx
@@ -4,35 +4,56 @@ import logo from "../assets/Logo.jpeg";
 import Carousel from "../Carousel.jsx";
 
 const AzuraAnimation = () => {
-    const [isAnimating, setIsAnimating] = useState(true);
-    const [showCarousel, setShowCarousel] = useState(false);
-  
-    useEffect(() => {
-      const animationTimeout = setTimeout(() => {
-        setIsAnimating(false);
-      }, 10);
-  
-      const carouselTimeout = setTimeout(() => {
-        setShowCarousel(true);
-      }, 4000);
-  
-      return () => {
-        clearTimeout(animationTimeout);
-        clearTimeout(carouselTimeout);
-      };
-    }, []);
-  
-    return (
-      <div className={`azura-container ${isAnimating ? "fade-in" : ""}`}>
-        {isAnimating && (
-          <div className="azura-text">
-            <img src={logo} alt="azuralogo" className="logo" />
-            AZURA
+  const [isAnimating, setIsAnimating] = useState(true);
+  const [showCarousel, setShowCarousel] = useState(false);
+
+  useEffect(() => {
+    const animationTimeout = setTimeout(() => {
+      setIsAnimating(true);
+    }, 10);
+
+    const carouselTimeout = setTimeout(() => {
+      setShowCarousel(true);
+    }, 10000);
+
+    return () => {
+      clearTimeout(animationTimeout);
+      clearTimeout(carouselTimeout);
+    };
+  }, []);
+
+  return (
+    <>
+      {isAnimating && (
+        <div className="azura-container">
+          <div className="azura-text-container">
+          <img src={logo} alt="azuralogo" className="logoAnimation" />
+            <div className={`azura-text fade-in`}>
+             
+              A
+            </div>
+            <div className={`azura-text fade-in delay-1`}>
+             
+              Z
+            </div>
+            <div className={`azura-text fade-in delay-2`}>
+              
+              U
+            </div>
+            <div className={`azura-text fade-in delay-3`}>
+              
+              R
+            </div>
+            <div className={`azura-text fade-in delay-4`}>
+            
+              A
+            </div>
           </div>
-        )}
-        {showCarousel && !isAnimating && <Carousel />}
-      </div>
-    );
-  };
-  
+        </div>
+      )}
+      {showCarousel && !isAnimating && <Carousel />}
+    </>
+  );
+};
+
 export default AzuraAnimation;
